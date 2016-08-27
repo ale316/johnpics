1590efc5a61789ed78dd4c30010e32ead88962d2
Alessandro Marin <alessandro.m316@gmail.com>
2016-08-27 22:32:20 +0000

    now clicking on image will refresh rather than using a button

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,5 +1,15 @@
-var $pic = document.querySelector('.more-john')
+var $pic = document.querySelector('.pic')
 // &#127881 &#128586 &#127881
 $pic.addEventListener('click', function() {
 	window.location.reload()
 })
+
+function pastelColor(){
+    var r = (Math.round(Math.random()* 127) + 127).toString(16);
+    var g = (Math.round(Math.random()* 127) + 127).toString(16);
+    var b = (Math.round(Math.random()* 127) + 127).toString(16);
+    return '#' + r + g + b;
+}
+
+var $instructions = document.querySelector('.instructions')
+$instructions.style.color = pastelColor()
